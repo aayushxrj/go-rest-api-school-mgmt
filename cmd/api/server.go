a3ca32b6a5faa5167970baa0748cfdf516215b73
aayushxrj <aayush.ranjan0904@gmail.com>
2025-09-01 22:38:40 +0530

    handle path/query y params

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,14 +2,91 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
+	"strings"
+
+	// "time"
 
 	mw "github.com/aayushxrj/go-rest-api-school-mgmt/internal/api/middlewares"
 	"golang.org/x/net/http2"
 )
+
+type Teacher struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Class     int
+	Subject   string
+}
+
+var (
+	teachers = make(map[int]Teacher)
+	// mutex    = &sync.Mutex{}  // use in the post method
+	nextID = 1
+)
+
+func init() {
+	teachers[nextID] = Teacher{ID: nextID, FirstName: "John", LastName: "Doe", Class: 10, Subject: "Math"}
+	nextID++
+	teachers[nextID] = Teacher{ID: nextID, FirstName: "Jane", LastName: "Smith", Class: 9, Subject: "Science"}
+	nextID++
+	teachers[nextID] = Teacher{ID: nextID, FirstName: "Jane", LastName: "Doe", Class: 11, Subject: "Biology"}
+}
+
+func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Path Parameters
+	pathStr := strings.TrimPrefix(r.URL.Path, "/teachers/")
+	idStr := strings.TrimSuffix(pathStr, "/")
+	fmt.Println("ID String:", idStr)
+
+	// Query Parameters
+	if idStr == "" {
+		first_name := r.URL.Query().Get("first_name")
+		last_name := r.URL.Query().Get("last_name")
+
+		teacherList := make([]Teacher, 0, len(teachers))
+		for _, teacher := range teachers {
+			if (first_name == "" || first_name == teacher.FirstName) && (last_name == "" || last_name == teacher.LastName) {
+				teacherList = append(teacherList, teacher)
+			}
+		}
+
+		response := struct {
+			Status string    `json:"status"`
+			Count  int       `json:"count"`
+			Data   []Teacher `json:"data"`
+		}{
+			Status: "success",
+			Count:  len(teacherList),
+			Data:   teacherList,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response) 
+		return
+	}
+
+	// Handle Path Parameter
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Teacher ID", http.StatusBadRequest)
+		return
+	}
+
+	teacher, exists := teachers[id]
+	if !exists {
+		http.Error(w, "Teacher Not Found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(teacher) 
+}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Root Route"))
@@ -19,7 +96,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		w.Write([]byte("GET Method on Teachers Route"))
+		getTeachersHandler(w, r)
 	case http.MethodPost:
 		w.Write([]byte("POST Method on Teachers Route"))
 	case http.MethodPut:
@@ -92,25 +169,25 @@ func main() {
 	}
 
 	// rate limiter
-	rl := mw.NewRateLimiter(5, 1*time.Minute)
+	// rl := mw.NewRateLimiter(5, 1*time.Minute)
 
-	hppOptions := mw.HPPOptions{
-		CheckQuery:                  true,
-		CheckBody:                   true,
-		CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
-		Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
-	}
+	// hppOptions := mw.HPPOptions{
+	// 	CheckQuery:                  true,
+	// 	CheckBody:                   true,
+	// 	CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
+	// 	Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
+	// }
 
 	// proper ordering of middlewares
 	// example: Cors -> Rate Limiter -> Response Time -> Security Headers -> Compression -> HPP -> Actual Handler
 	// secureMux := mw.Cors(rl.Middleware(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Compression(mw.Hpp(hppOptions)(mux))))))
 	secureMux := applyMiddlewares(mux,
-		mw.Hpp(hppOptions),
-		mw.Compression,
+		// mw.Hpp(hppOptions),
+		// mw.Compression,
 		mw.SecurityHeaders,
-		mw.ResponseTimeMiddleware,
-		rl.Middleware,
-		mw.Cors,
+		// mw.ResponseTimeMiddleware,
+		// rl.Middleware,
+		// mw.Cors,
 	)
 
 	// Create custom server
